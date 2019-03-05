d24aa0d7639739eef84448e7d5000e1e760997a7
Ritikesh <ritikeshsisodiya@gmail.com>
2019-03-06 02:06:34 +0530

    Commit version 1.0.0

---
+++ README.md
@@ -0,0 +1,51 @@
+# MemoizeUntil
+
+A unique Memoization Pattern which memoizes until the next time metric - could be until the next minute, hour, day, week.
+
+## Get Started
+
+Install this grunt plugin next to your project's grunt.js gruntfile with: `npm install memoize_until --save-dev`
+
+## Usage
+
+```javascript
+const MemoizeUntil = require('./memoize_until.js').MemoizeUntil
+
+MemoizeUntil.fetch('min', 'default', () => { 
+    return 'SomeComplexOperation'; 
+})
+```
+
+#### options:
+1. `min` : Metric until when you want to memoize. Can be one of ['min', 'hour', 'day', 'week']
+
+2. `default` : All metric levels come with a 'default' key. More can be added by [initializing](#Initialisation) `MemoizeUntil` with extra keys
+
+3. `cb`: callback function. This callback function must return a value which needs to be memoized/cached.
+
+## Initialisation
+To add custom keys to each metric, pass a custom object to the `init` function of `MemoizeUntil`.
+```javascript
+MemoizeUntil.init({ 
+    min: ['custom1', 'custom2']
+})
+
+MemoizeUntil.fetch('min', 'custom1', () => { 
+    return 'SomeComplexOperation'; 
+})
+```
+
+## Extend
+To add custom keys during runtime to any metric, use the `extend` function of `MemoizeUntil`.
+```javascript
+let runtime_key = 'runtime_key';
+MemoizeUntil.extend('min', runtime_key)
+
+MemoizeUntil.fetch('min', runtime_key, () => { 
+    return 'SomeComplexOperation'; 
+})
+```
+
+## License
+[Ritikesh](https://ritikesh.github.io)  
+Licensed under the MIT license.
---
+++ memoize_until.js
@@ -0,0 +1,75 @@
+"use strict";
+exports.__esModule = true;
+var NullObject = /** @class */ (function () {
+    function NullObject() {
+    }
+    return NullObject;
+}());
+var Store = /** @class */ (function () {
+    function Store(_moment) {
+        this._moment = _moment;
+        this._store = {};
+        this._null = new NullObject();
+        this.extend('default');
+    }
+    Store.prototype.fetch = function (key, cb) {
+        var value = this.get(key);
+        if (value === undefined) {
+            this.clearAll(key);
+            value = this.set(key, cb());
+        }
+        return this.unsetNull(value);
+    };
+    Store.prototype.extend = function (key) {
+        if (!this._store[key]) {
+            this.clearAll(key);
+        }
+    };
+    Store.prototype.get = function (key) {
+        var moment = this._moment();
+        return this._store[key][moment];
+    };
+    Store.prototype.set = function (key, value) {
+        var moment = this._moment();
+        return this._store[key][moment] = this.setNull(value);
+    };
+    Store.prototype.clearAll = function (key) {
+        this._store[key] = {};
+    };
+    Store.prototype.setNull = function (value) {
+        if (value === undefined || value === null || value === NaN) {
+            return this._null;
+        }
+        return value;
+    };
+    Store.prototype.unsetNull = function (value) {
+        if (value instanceof NullObject) {
+            return undefined;
+        }
+        return value;
+    };
+    return Store;
+}());
+var MemoizeMinute = new Store(function () { return (new Date()).getMinutes(); });
+var MemoizeHour = new Store(function () { return (new Date()).getHours(); });
+var MemoizeDay = new Store(function () { return (new Date()).getDate(); });
+var MemoizeWeek = new Store(function () { return (new Date()).getDay(); });
+var Map = {
+    min: MemoizeMinute,
+    hour: MemoizeHour,
+    day: MemoizeDay,
+    week: MemoizeWeek
+};
+var MemoizeUntil = {
+    init: function (keyset) {
+        for (var kind in keyset) {
+            for (var key in keyset[kind]) {
+                Map[kind].extend(key);
+            }
+        }
+    },
+    fetch: function (kind, key, cb) {
+        return Map[kind].fetch(key, cb);
+    }
+};
+exports.MemoizeUntil = MemoizeUntil;
